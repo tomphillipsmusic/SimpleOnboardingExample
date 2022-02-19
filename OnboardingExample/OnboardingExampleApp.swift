8b028f657ec94bfdb5d13631a01f70b53ae66a43
Tom Phillips <tomphillipsmusic@gmail.com>
2022-02-19 09:40:30 -0500

    Initial Commit

---
+++ OnboardingExample/OnboardingExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingExampleApp.swift
+//  OnboardingExample
+//
+//  Created by Tom Phillips on 2/19/22.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
